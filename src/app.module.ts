b47600755c6c36573a3d75d94c3e396ab543473e
lesytruong <lesytruong247@gmail.com>
2024-12-16 06:02:49 +0700

    check mysql data

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { InterceptorsModule } from '@/interceptors/interceptor.module'
 import { Inject, Module, OnModuleInit } from '@nestjs/common'
 import { PRODUCT_REPOSITORY } from './api/products/product.di-token'
 import { ProductsModule } from './api/products/products.module'
+import { products } from './mockdata/products'
+import { ImageColor } from './api/products/entities/models/imageColor.entity'
+import { Product } from './api/products/entities/models/product.entity'
 
 @Module({
   imports: [
@@ -23,27 +26,27 @@ export class AppModule implements OnModuleInit {
   ) {}
 
   async onModuleInit() {
-    // products.map((item) => {
-    //   try {
-    //     const color_images = item.images_color.map(
-    //       (item) => new ImageColor({ url: item.url, color: item.color })
-    //     )
-    //
-    //     const product = new Product({
-    //       name: item.name,
-    //       price: item.price,
-    //       category: item.category,
-    //       currency: item.currency,
-    //       sizes: item.sizes,
-    //       imagesColor: color_images,
-    //       descriptionOfUse: item.descriptionOfUse,
-    //       features: item.features
-    //     })
-    //
-    //     this.createProductRepo.create(product)
-    //   } catch (error) {
-    //     console.log(error.stack)
-    //   }
-    // })
+    products.map((item) => {
+      try {
+        const color_images = item.images_color.map(
+          (item) => new ImageColor({ url: item.url, color: item.color })
+        )
+
+        const product = new Product({
+          name: item.name,
+          price: item.price,
+          category: item.category,
+          currency: item.currency,
+          sizes: item.sizes,
+          imagesColor: color_images,
+          descriptionOfUse: item.descriptionOfUse,
+          features: item.features
+        })
+
+        this.createProductRepo.create(product)
+      } catch (error) {
+        console.log(error.stack)
+      }
+    })
   }
 }
